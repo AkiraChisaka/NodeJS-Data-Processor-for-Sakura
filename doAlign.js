299a922e2c9781d413f76bed31750e7dd2dad04b
AkiraChisaka <steinsraven@gmail.com>
2024-03-14 19:05:49 -0700

    Add file read and write functionality to doAlign.js

--- doAlign.js
+++ doAlign.js
@@ -1,5 +1,23 @@
 require("dotenv").config()
 
-let myVariable = process.env.MY_VARIABLE
+const DEFAULT_JP_FILE_LOCATION = process.env.DEFAULT_JP_FILE_LOCATION
 
-console.log(myVariable)
+const fs = require("fs")
+
+// To read a file
+fs.readFile(DEFAULT_JP_FILE_LOCATION, "utf8", (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.log(data)
+
+    // To write to a file
+    fs.writeFile(DEFAULT_JP_FILE_LOCATION, "Your content here", (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+    })
+})
