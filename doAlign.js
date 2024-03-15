eb2916b169d5e87f3f031dd1cb7abe432d872331
AkiraChisaka <steinsraven@gmail.com>
2024-03-14 19:15:59 -0700

    Switched to synced version of read and write

--- doAlign.js
+++ doAlign.js
@@ -5,19 +5,31 @@ const DEFAULT_JP_FILE_LOCATION = process.env.DEFAULT_JP_FILE_LOCATION
 const fs = require("fs")
 
 // To read a file
-fs.readFile(DEFAULT_JP_FILE_LOCATION, "utf8", (err, data) => {
-    if (err) {
-        console.error(err)
-        return
-    }
+// fs.readFile(DEFAULT_JP_FILE_LOCATION, "utf8", (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     console.log(data)
+
+//     // To write to a file
+//     fs.writeFile(DEFAULT_JP_FILE_LOCATION, "Your content here", (err) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }
+//         //file written successfully
+//     })
+// })
+
+try {
+    // To read the file
+    const data = fs.readFileSync(DEFAULT_JP_FILE_LOCATION, "utf8")
     console.log(data)
 
-    // To write to a file
-    fs.writeFile(DEFAULT_JP_FILE_LOCATION, "Your content here", (err) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        //file written successfully
-    })
-})
+    // To write the file
+    fs.writeFileSync(DEFAULT_JP_FILE_LOCATION, "Your content here")
+    //file written successfully
+} catch (err) {
+    console.error(err)
+}
