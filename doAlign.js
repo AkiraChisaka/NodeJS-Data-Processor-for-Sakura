2be7b9796e3cd807dd5dfd07f5b8889f959f5a8d
AkiraChisaka <steinsraven@gmail.com>
2024-03-15 21:24:45 -0700

    Can now read a whole folder

--- doAlign.js
+++ doAlign.js
@@ -1,50 +1,71 @@
 require("dotenv").config()
 const fs = require("fs")
-const { JP_INPUT_LOCATION, CN_INPUT_LOCATION, DI_INPUT_LOCATION, OUTPUT_LOCATION } = process.env
+const path = require("path")
+const { JP_INPUT_LOCATION, CN_INPUT_LOCATION, DI_INPUT_LOCATION, INPUT_LOCATION, OUTPUT_LOCATION } = process.env
 
-let outputJSON = {
-    id_novel: 1,
-    gpt_dict: [
-        // Examples
-        // {
-        //     src: "",
-        //     dst: "",
-        //     info: "",
-        // },
-        // {
-        //     src: "",
-        //     dst: "",
-        //     info: "",
-        // },
-    ],
-    data: [
-        // Examples
-        // {
-        //     id_line: 1,
-        //     ja_text: "xxxx",
-        //     zh_text: "yyyy",
-        // },
-        // {
-        //     id_line: 2,
-        //     ja_text: "xxxx",
-        //     zh_text: "yyyy",
-        // },
-    ],
-}
+// let outputJSON = {
+//     id_novel: 1,
+//     gpt_dict: [
+//         // Examples
+//         // {
+//         //     src: "",
+//         //     dst: "",
+//         //     info: "",
+//         // },
+//         // {
+//         //     src: "",
+//         //     dst: "",
+//         //     info: "",
+//         // },
+//     ],
+//     data: [
+//         // Examples
+//         // {
+//         //     id_line: 1,
+//         //     ja_text: "xxxx",
+//         //     zh_text: "yyyy",
+//         // },
+//         // {
+//         //     id_line: 2,
+//         //     ja_text: "xxxx",
+//         //     zh_text: "yyyy",
+//         // },
+//     ],
+// }
 
 // Main
 
-let jpText = fs.readFileSync(JP_INPUT_LOCATION, "utf8")
-let cnText = fs.readFileSync(CN_INPUT_LOCATION, "utf8")
-let diText = fs.readFileSync(DI_INPUT_LOCATION, "utf8")
+// let jpText = fs.readFileSync(JP_INPUT_LOCATION, "utf8")
+// let cnText = fs.readFileSync(CN_INPUT_LOCATION, "utf8")
+// let diText = fs.readFileSync(DI_INPUT_LOCATION, "utf8")
 
-// console.log(jpText)
+// // console.log(jpText)
 
-outputJSON.data.push(...convertTextToJSON(jpText, cnText))
+// outputJSON.data.push(...convertTextToJSON(jpText, cnText))
 
-outputJSON.gpt_dict.push(...convertDictToJSON(diText))
+// outputJSON.gpt_dict.push(...convertDictToJSON(diText))
 
-fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(outputJSON, null, 2), "utf8")
+// fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(outputJSON, null, 2), "utf8")
+
+let filenames = fs.readdirSync(INPUT_LOCATION)
+let novelNames = [...new Set(filenames.map((filename) => filename.slice(0, -7)))]
+
+let novels = novelNames.map((novelName, index) => {
+    let jpText = fs.readFileSync(path.join(INPUT_LOCATION, `${novelName} JP.txt`), "utf8")
+    let cnText = fs.readFileSync(path.join(INPUT_LOCATION, `${novelName} CN.txt`), "utf8")
+    let diText = fs.readFileSync(path.join(INPUT_LOCATION, `${novelName} DI.txt`), "utf8")
+
+    let data = convertTextToJSON(jpText, cnText)
+    let gpt_dict = convertDictToJSON(diText)
+
+    return {
+        id_novel: index + 1,
+        gpt_dict: gpt_dict,
+        data: data,
+    }
+})
+
+fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(novels, null, 2), "utf8")
 
 // End of main
 
