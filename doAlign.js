c0e2c68c904d3b32ae741f19b137472c50eca13a
AkiraChisaka <steinsraven@gmail.com>
2024-03-15 21:39:53 -0700

    Added some metadata and line counting

--- doAlign.js
+++ doAlign.js
@@ -3,49 +3,7 @@ const fs = require("fs")
 const path = require("path")
 const { JP_INPUT_LOCATION, CN_INPUT_LOCATION, DI_INPUT_LOCATION, INPUT_LOCATION, OUTPUT_LOCATION } = process.env
 
-// let outputJSON = {
-//     id_novel: 1,
-//     gpt_dict: [
-//         // Examples
-//         // {
-//         //     src: "",
-//         //     dst: "",
-//         //     info: "",
-//         // },
-//         // {
-//         //     src: "",
-//         //     dst: "",
-//         //     info: "",
-//         // },
-//     ],
-//     data: [
-//         // Examples
-//         // {
-//         //     id_line: 1,
-//         //     ja_text: "xxxx",
-//         //     zh_text: "yyyy",
-//         // },
-//         // {
-//         //     id_line: 2,
-//         //     ja_text: "xxxx",
-//         //     zh_text: "yyyy",
-//         // },
-//     ],
-// }
-
 // Main
-
-// let jpText = fs.readFileSync(JP_INPUT_LOCATION, "utf8")
-// let cnText = fs.readFileSync(CN_INPUT_LOCATION, "utf8")
-// let diText = fs.readFileSync(DI_INPUT_LOCATION, "utf8")
-
-// // console.log(jpText)
-
-// outputJSON.data.push(...convertTextToJSON(jpText, cnText))
-
-// outputJSON.gpt_dict.push(...convertDictToJSON(diText))
-
-// fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(outputJSON, null, 2), "utf8")
 
 let filenames = fs.readdirSync(INPUT_LOCATION)
 let novelNames = [...new Set(filenames.map((filename) => filename.slice(0, -7)))]
@@ -55,17 +13,24 @@ let novels = novelNames.map((novelName, index) => {
     let cnText = fs.readFileSync(path.join(INPUT_LOCATION, `${novelName} CN.txt`), "utf8")
     let diText = fs.readFileSync(path.join(INPUT_LOCATION, `${novelName} DI.txt`), "utf8")
 
-    let data = convertTextToJSON(jpText, cnText)
+    let text_data = convertTextToJSON(jpText, cnText)
     let gpt_dict = convertDictToJSON(diText)
 
     return {
         id_novel: index + 1,
+        line_count: text_data.length,
         gpt_dict: gpt_dict,
-        data: data,
+        text_data: text_data,
     }
 })
 
-fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(novels, null, 2), "utf8")
+let outputJSON = {
+    novel_count: novels.length,
+    line_count_total: novels.reduce((total, novel) => total + novel.line_count, 0),
+    data: novels,
+}
+
+fs.writeFileSync(OUTPUT_LOCATION, JSON.stringify(outputJSON, null, 2), "utf8")
 
 // End of main
 
